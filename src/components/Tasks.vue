213933b756cd666b94bffd032aa11f5df06dfbc3
kennyOlakunle <olakunlemuyiwa@gmail.com>
2021-02-25 22:48:40 +0100

    init

---
+++ src/components/Tasks.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="space" v-for="task in tasks" :key="task.id">
+    <Task :task="task" />
+</div>
+</template>
+
+<script>
+import Task from './Task'
+export default {
+name: 'Tasks',
+components: {
+    Task
+}, 
+props: {
+    tasks: Array
+}
+}
+</script>
+
+<style scoped>
+
+.space {
+    margin-bottom: 5px;
+}
+</style>
